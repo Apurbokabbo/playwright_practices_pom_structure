22439bc4fe198edd3815313e60d7fa7fc66f8827
Apurbo_Kabbo <apurbokabbo1999@gmail.com>
2026-07-16 00:35:39 +0600

    utils config

---
+++ utils/helpers.py
@@ -0,0 +1,31 @@
+import logging
+from config.base_config import BaseConfig
+from pathlib import Path
+from datetime import datetime
+
+def take_screenshot(page, name):
+    try:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        path = Path(BaseConfig.SCREENSHOT_DIR)
+        path.mkdir(parents=True, exist_ok=True)
+        file_path = path / f"{name}_{timestamp}.png"
+        page.wait_for_load_state("networkidle", timeout=10000)
+        page.screenshot(
+            path=str(file_path),
+            timeout=60000,
+            full_page=True
+        )
+        return file_path   # <-- IMPORTANT
+    except Exception as e:
+        logging.exception(f"Failed to take screenshot: {e}")
+        return None
+
+def highlight_element(page, selector, color="yellow", duration=0.5):
+    page.eval_on_selector(
+        selector,
+        f"""(el) => {{
+            const original = el.style.border;
+            el.style.color = "2px solid {color}";
+            setTimeout(()=> el.style.border = original, {int(duration * 1000)});
+        }}"""
+    )
---
+++ utils/logger.py
@@ -0,0 +1,29 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+from time import asctime
+
+from config.base_config import BaseConfig
+
+
+def setup_logger(name: str , level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if logger.Handlers:
+        return logger
+
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_path = Path(BaseConfig.LOG_PATH)
+    log_path.mkdir(parents=True, exist_ok=True)
+
+    file_handler = logging.FileHandler(log_path / f"{name}_{datetime.now()}.strftime('%Y-%m-%d_%H-%M-%S').log")
+    file_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
