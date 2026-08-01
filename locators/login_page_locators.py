96cd7c5b087a419868dab20d8dd772d24ffa1116
Apurbo_Kabbo <apurbokabbo1999@gmail.com>
2026-08-01 23:49:20 +0600

    login page locator added

---
+++ locators/login_page_locators.py
@@ -0,0 +1,5 @@
+class LoginPageLocators:
+    USER_NAME = "//*[@data-test='username']"
+    PASSWORD = "//*[@data-test='password']"
+    LOGIN_BUTTON = "//*[@data-test='login-button']"
+    ERROR = "//*[@data-test='error']"
